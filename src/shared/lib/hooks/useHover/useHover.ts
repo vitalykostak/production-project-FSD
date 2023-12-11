bc58bd8b58ec287f16c293d051b90c1af51f9cea
vitalykostak <vitalykostak@gmail.com>
2023-12-11 13:37:13 +0100

    Implement shared/lib/hooks/useHover - useHover

---
+++ src/shared/lib/hooks/useHover/useHover.ts
@@ -0,0 +1,17 @@
+import { useCallback, useState, type MouseEventHandler } from 'react'
+
+interface BindHover {
+  onMouseEnter: MouseEventHandler<HTMLElement>
+  onMouseLeave: MouseEventHandler<HTMLElement>
+}
+
+type UseHoverResult = [isHover:boolean, BindHover]
+
+export const useHover = (): UseHoverResult => {
+  const [isHover, setIsHover] = useState<boolean>(false)
+
+  const onMouseEnter: MouseEventHandler<HTMLElement> = useCallback((e) => setIsHover(true), [])
+  const onMouseLeave: MouseEventHandler<HTMLElement> = useCallback((e) => setIsHover(false), [])
+
+  return [isHover, { onMouseEnter, onMouseLeave }]
+}
