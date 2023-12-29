59ed93ebd32ede35d9246154a463f3ed93ecacbe
vitalykostak <vitalykostak@gmail.com>
2023-12-29 10:24:13 +0100

    Implement useModal for enhanced functionality in refactoring Drawer and Modal components

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -1,16 +1,10 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import styles from './Drawer.module.scss'
-import {
-  type ReactNode,
-  type FC,
-  useRef,
-  useEffect,
-  useState,
-  useCallback
-} from 'react'
+import { type ReactNode, type FC } from 'react'
 import { useTheme } from 'app/providers/ThemeProvider'
 import Portal from '../Portal/Portal'
 import Overlay from '../Overlay/Overlay'
+import { useModal } from 'shared/lib/hooks/useModal/useModal'
 
 interface ModalProps {
   className?: string
@@ -27,44 +21,11 @@ const Drawer: FC<ModalProps> = (props) => {
 
   const { theme } = useTheme()
 
-  const [isMounted, setIsMounted] = useState<boolean>(false)
-  const [isClosing, setIsClosing] = useState<boolean>(false)
-  const timerRef = useRef<ReturnType<typeof setTimeout>>()
-
-  const closeHandler = useCallback(() => {
-    if (onClose) {
-      setIsClosing(true)
-      timerRef.current = setTimeout(() => {
-        onClose()
-        setIsClosing(false)
-      }, ANIMATION_DELAY)
-    }
-  }, [onClose])
-
-  const onCloseKeydown = useCallback(
-    (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        closeHandler()
-      }
-    },
-    [closeHandler]
-  )
-
-  useEffect(() => {
-    if (isOpen) {
-      window.addEventListener('keydown', onCloseKeydown)
-    }
-
-    return () => {
-      clearTimeout(timerRef.current)
-    }
-  }, [onCloseKeydown, isOpen])
-
-  useEffect(() => {
-    if (isOpen && !isMounted) {
-      setIsMounted(true)
-    }
-  }, [isOpen, isMounted])
+  const { isClosing, close, isMounted } = useModal({
+    onClose,
+    isOpen,
+    animationDelay: ANIMATION_DELAY
+  })
 
   const mods = {
     [styles.opened]: isOpen,
@@ -81,10 +42,8 @@ const Drawer: FC<ModalProps> = (props) => {
   return (
     <Portal>
       <div className={classNames(styles.Drawer, mods, additionsClasses)}>
-        <Overlay onClick={closeHandler}/>
-          <div className={styles.content}>
-            {children}
-          </div>
+        <Overlay onClick={close} />
+        <div className={styles.content}>{children}</div>
       </div>
     </Portal>
   )
