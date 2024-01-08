ea4537fbe8f8cc50c75857f930cbe1998e9cb9bb
vitalykostak <vitalykostak@gmail.com>
2024-01-08 21:08:18 +0100

    Refactor buildSelector helper

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -2,11 +2,15 @@ import { useSelector } from 'react-redux'
 
 import { type StateSchema } from '@/app/providers/StoreProvider'
 
-type Selector<T> = (state: StateSchema) => T
-type BuildSelectorResult<T> = [() => T, Selector<T>]
+type Selector<T, Args extends any[]> = (state: StateSchema, ...args: Args) => T
+type Hook<T, Args extends any[]> = (...args: Args) => T
+type BuildSelectorResult<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>]
 
-export const buildSelector = <T>(selector: Selector<T>): BuildSelectorResult<T> => {
-    const useCustomSelector = () => useSelector(selector)
+export const buildSelector = <T, Args extends any[]>(
+    selector: Selector<T, Args>,
+): BuildSelectorResult<T, Args> => {
+    const useCustomSelector: Hook<T, Args> = (...args) =>
+        useSelector((state: StateSchema) => selector(state, ...args))
 
     return [useCustomSelector, selector]
 }
